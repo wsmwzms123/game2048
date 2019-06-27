c40c9f6160fd453de1c56dd71327939ca25e1bde
homerWong <wyg_098123@163.com>
2019-06-27 11:17:32 +0800

    refactor: remove core codes

--- src/instance/index.js
+++
@@ -1,8 +0,0 @@
-import init from './init'
-class Game2048 {
-  constructor (options) {
-    this._init(options)
-  }
-}
-init(Game2048)
-export default Game2048
--- src/instance/init-state.js
+++
@@ -1,6 +0,0 @@
-import initStorage from './init-storage'
-export default function (gm) {
-  initStorage(gm)
-  gm.gameState = gm.gameState ||
-   Array.from({ length: 4 }, () => [...Array(4).fill(null)])
-}
